c0dc1ca744a96af0d4f1e74892757a3478768711
dingfeng <fengDing0321@gmail.com>
2024-11-30 01:02:33 +0800

    minor changes

--- src/components/Posts/Compilations.js
+++ src/components/Posts/Compilations.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import postsData from '../../data/posts/posts.json';
+import compilationData from '../../data/posts/compilations.json';
 
 const Compilations = () => {
   const categories = postsData.reduce((acc, post) => {
-    const category = post.category || 'General';
+    const category = post.category || 'Uncategorized';
     if (!acc[category]) acc[category] = [];
     acc[category].push(post);
     return acc;
@@ -17,18 +18,21 @@ const Compilations = () => {
         <h2>Compilations</h2>
       </div>
       <div className="compilation-container">
-        {Object.entries(categories).map(([category]) => (
-          <nav className="links">
-            <div className="compilation-box">
-              <Link to="/">
-                <h3>{category}</h3>
-                <p> Introduction to this category.
-                  Introduction.
-                </p>
-              </Link>
-            </div>
-          </nav>
-        ))}
+        {Object.entries(categories).map(([category]) => {
+          const compilation = compilationData.find(
+            (cat) => cat.title.trim().toLowerCase() === category.trim().toLowerCase(),
+          );
+          return (
+            <nav className="links">
+              <div className="compilation-box">
+                <Link to="/">
+                  <h3>{category}</h3>
+                  <p>{compilation ? compilation.description : 'No description available.'}</p>
+                </Link>
+              </div>
+            </nav>
+          );
+        })}
       </div>
     </div>
   );
