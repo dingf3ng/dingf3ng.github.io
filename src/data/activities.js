58d3d12d7b20aa64cc250212238fb34cf47a9166
dingfeng <fengDing0321@gmail.com>
2025-11-23 16:28:36 +0800

    update

--- src/data/activities.js
+++ src/data/activities.js
@@ -1,9 +1,23 @@
 const activities = [
   {
     id: 1,
+    date: '2025-12',
+    title: 'winter break',
+    description: 'Beginning of winter break, I am going to have a great time relaxing and recharging!',
+    link: '/projects/tcs',
+  },
+  {
+    id: 1,
+    date: '2025-11',
+    title: 'pl-summit',
+    description: '<a href="https://sg-pl-summit.github.io">SG PL Summit</a> will be held in November 24, 2025 at Singapore Management University (SMU). We are going to present our work on dual programming (we are the only undergraduate group presenting). See you there!',
+    link: '/projects/tcs',
+  },
+  {
+    id: 1,
     date: '2025-11',
     title: 'Pie-web',
-    description: 'Our team has developed a web-based IDE for the Pie, an educational language introduced in The Little Typer. It is now available at <a href="https://source-academy.github.io/pie-slang/">https://source-academy.github.io/pie-slang/!</a>',
+    description: 'Our team has developed a web-based IDE for the Pie, an educational language introduced in The Little Typer. It is now available <a href="https://source-academy.github.io/pie-slang/">here</a>!',
     link: '/projects/tcs',
   },
   {
@@ -47,6 +61,13 @@ const activities = [
     title: 'Trip to Europe',
     description: 'From Paris to Instanbul, had a wonderful time exploring the culture and history of Europe!',
     link: '/blog/algo-complexity',
+  },
+  {
+    id: 1,
+    date: '2024-12',
+    title: 'pl-summit0',
+    description: '<a href="https://verse-lab.github.io/sg-pl-summit/">The first SG PL Summit</a> will be held in Dec 4th, 2024 at AStar. Our team will be presenting the "Pie in SourceAcademy" project.',
+    link: '/projects/tcs',
   },
 ];
 
